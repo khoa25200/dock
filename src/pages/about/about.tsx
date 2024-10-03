0589775f68df1c5502482dc5ac263f6d309de81f
NguyenHaAnh <haanhnguyen1221@gmail.com>
2024-10-03 09:58:00 +0700

    create: about page

---
+++ src/pages/about/about.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './about.less';
+import LayoutLandingPage from '../../layouts/LayoutLandingPage/LayoutLandingPage';
+const AboutPage: React.FC = () => {
+  return (
+    <LayoutLandingPage>
+      <h1>About Us</h1>
+      <p>Welcome to our About page!</p>
+    </LayoutLandingPage>
+  );
+};
+
+export default AboutPage;
