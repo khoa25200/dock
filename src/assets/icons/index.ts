6b9d1a230ce3cd2df384afb460dfb8160496f86a
NguyenHaAnh <haanhnguyen1221@gmail.com>
2024-10-04 10:30:30 +0700

    Add: index file to icons folder

---
+++ src/assets/icons/index.ts
@@ -0,0 +1,5 @@
+export const ICONS = {
+  HAPPY: new URL('./happy.svg', import.meta.url).href,
+  GUARD: new URL('./guard.svg', import.meta.url).href,
+  TIME: new URL('./time.svg', import.meta.url).href,
+};
