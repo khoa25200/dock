74f7328ceeb7ba2a55701c0683e24ba00f257ce5
Quach Hai Bang <68978560+bangqh123@users.noreply.github.com>
2024-10-04 11:39:30 +0700

    fix grammar errors for introduce

--- src/pages/home/sections/Introduce/Intruduce.tsx
+++ src/pages/home/sections/Introduce/Intruduce.tsx
@@ -6,7 +6,6 @@ type IntroduceLayoutProps = {
   description: string;
   img: string;
   imgDesctions: string,
-  iconQuote: string;
 };
 
 const IntroduceLayout: React.FC<IntroduceLayoutProps> = ({
@@ -14,12 +13,10 @@ const IntroduceLayout: React.FC<IntroduceLayoutProps> = ({
   description,
   img,
   imgDesctions,
-  iconQuote,
 }: IntroduceLayoutProps) => {
   return (
     <section className="introduce">
       <div className="introduce-content">
-        <img src={iconQuote} alt="Double quote" />
         <span
           className="introduce-content-cause"
           dangerouslySetInnerHTML={{ __html: title }}></span>
