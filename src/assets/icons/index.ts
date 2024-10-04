c69e7bce6154d73e67fb1097c724bce0ccb316ef
Quach Hai Bang <68978560+bangqh123@users.noreply.github.com>
2024-10-04 11:08:54 +0700

    fix the link icon and images path default

--- src/assets/icons/index.ts
+++ src/assets/icons/index.ts
@@ -2,4 +2,5 @@ export const ICONS = {
   HAPPY: new URL('./happy.svg', import.meta.url).href,
   GUARD: new URL('./guard.svg', import.meta.url).href,
   TIME: new URL('./time.svg', import.meta.url).href,
+  DOUBLE_QUOTE: new URL('./double_quote.svg', import.meta.url).href,
 };
