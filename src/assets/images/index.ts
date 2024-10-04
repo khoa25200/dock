e87bc77e7974197bf2b32d1260801450f71e5c55
NguyenHaAnh <haanhnguyen1221@gmail.com>
2024-10-04 10:30:38 +0700

    Add: index file to images folder

---
+++ src/assets/images/index.ts
@@ -0,0 +1,6 @@
+export const IMAGES = {
+  SHOWCASE: new URL('./message-showcase.png', import.meta.url).href,
+  LOGO: new URL('./logo-dock.png', import.meta.url).href,
+  WORKSPACE: new URL('./workspace-img.png', import.meta.url).href,
+  ABOUT_US: new URL('./about-us.png', import.meta.url).href,
+};
