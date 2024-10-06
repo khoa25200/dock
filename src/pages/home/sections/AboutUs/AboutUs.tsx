148a7cafbf63efeb08efbe474c33fc9452d1846f
NguyenHaAnh <haanhnguyen1221@gmail.com>
2024-10-06 19:36:56 +0700

    Style: About us section

--- src/pages/home/sections/AboutUs/AboutUs.tsx
+++ src/pages/home/sections/AboutUs/AboutUs.tsx
@@ -1,5 +1,5 @@
 import './AboutUs.less';
-
+import './AboutUs.media.less';
 type AboutUsProps = {
   title: string;
   description: string;
@@ -13,7 +13,9 @@ function AboutUs({ title, description, img, team }: AboutUsProps) {
       <h3 className="about-us-title">{title}</h3>
       <p className="about-us-description">{description}</p>
       <div className="about-us-content">
-        <div className="about-us-image" style={{ backgroundImage: `url(${img})` }}></div>
+        <div className="about-us-image">
+          <img src={img} alt="about-us" />
+        </div>
         <p className="about-us-team">{team}</p>
       </div>
     </section>
