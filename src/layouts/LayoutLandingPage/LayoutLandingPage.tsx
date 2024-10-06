4990d01494370bbd6de3ec998368e060671d6165
NguyenHaAnh <haanhnguyen1221@gmail.com>
2024-10-06 12:39:50 +0700

    Update: listen user scrolling on landing page

--- src/layouts/LayoutLandingPage/LayoutLandingPage.tsx
+++ src/layouts/LayoutLandingPage/LayoutLandingPage.tsx
@@ -1,12 +1,34 @@
 import FooterLayout from '../../layouts/Footer/Foooter';
 import HeaderComponent from '../Header/Header';
-import { PropsWithChildren } from 'react';
+import { PropsWithChildren, useRef, useState, useEffect } from 'react';
 import './LayoutLandingPage.less';
 
 function LayoutLandingPage({ children }: PropsWithChildren) {
+  const [isScroll, setIsScroll] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollTop = window.scrollY;
+      const docHeight = document.documentElement.scrollHeight - window.innerHeight;
+      const scrollFraction = scrollTop / docHeight;
+
+      if (scrollFraction >= 0.1) {
+        setIsScroll(true);
+      } else {
+        setIsScroll(false);
+      }
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
   return (
     <div className="layout-landing-page">
-      <HeaderComponent />
+      <HeaderComponent isScroll={isScroll} />
       {children}
       <FooterLayout />
     </div>
