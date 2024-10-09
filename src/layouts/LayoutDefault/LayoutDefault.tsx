f0d3e5c46bfa92b9cfff77cedefb5eee214d2267
Nguyễn Long Duy <88131939+Duy2401@users.noreply.github.com>
2024-10-09 14:26:09 +0700

    fix: space in component LayoutDefault

--- src/layouts/LayoutDefault/LayoutDefault.tsx
+++ src/layouts/LayoutDefault/LayoutDefault.tsx
@@ -1,9 +1,7 @@
-import { PropsWithChildren, useState } from 'react';
+import { PropsWithChildren } from 'react';
 import './LayoutDefault.less';
-
-import { IMAGES } from '../../assets/images';
-import { ICONS } from '../../assets/icons';
 import SidebarLayout from '../Sidebar/Sidebar';
+
 function LayoutDefault({ children }: PropsWithChildren) {
   return (
     <div className="layout-default">
