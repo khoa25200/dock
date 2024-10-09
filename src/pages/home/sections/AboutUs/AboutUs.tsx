8fbb09e37b78f30fd4916cb42ebfb0d399f9d38b
NguyenHaAnh <haanhnguyen1221@gmail.com>
2024-10-09 10:02:55 +0700

    Style: Add gradient

--- src/pages/home/sections/AboutUs/AboutUs.tsx
+++ src/pages/home/sections/AboutUs/AboutUs.tsx
@@ -1,5 +1,5 @@
-import "./AboutUs.less";
-import "./AboutUs.media.less";
+import './AboutUs.less';
+import './AboutUs.media.less';
 type AboutUsProps = {
   title: string;
   description: string;
@@ -11,17 +11,12 @@ function AboutUs({ title, description, img, team }: AboutUsProps) {
   return (
     <section className="about-us">
       <div className="about-us-content">
-        <h3
-          className="about-us-title"
-          dangerouslySetInnerHTML={{ __html: title }}
-        ></h3>
-        <p
-          className="about-us-description"
-          dangerouslySetInnerHTML={{ __html: description }}
-        ></p>
+        <h3 className="about-us-title" dangerouslySetInnerHTML={{ __html: title }}></h3>
+        <p className="about-us-description" dangerouslySetInnerHTML={{ __html: description }}></p>
       </div>
       <div className="about-us-image">
         <img src={img} alt="Group picture" />
+        <div className="about-us-image-gradient"></div>
       </div>
       <span className="about-us-team">{team}</span>
     </section>
