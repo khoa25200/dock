71c01ebca19ed5a47ab6f4a42430924ab6a58c8a
Nguyễn Long Duy <88131939+Duy2401@users.noreply.github.com>
2024-10-11 16:58:20 +0700

    fix: layout message panel

--- src/pages/chat/components/MessagePanel/partial/Form_message/Form_message.tsx
+++ src/pages/chat/components/MessagePanel/partial/Form_message/Form_message.tsx
@@ -3,28 +3,10 @@ import './Form_message.less';
 import { IMAGES } from '../../../../../../assets/images';
 
 const FormMessage = () => {
-  // const [chat, setChat] = useState();
-  const [open, setOpen] = useState(false);
-  const [text, setText] = useState('');
-  // const [img, setImg] = useState({
-  //   file: null,
-  //   url: '',
-  // });
-  console.log(open);
-
   return (
-    <div className="chat">
-      <div className="center">
-        <div className="message">
-          <img src={IMAGES.AVT_TEXT} />
-          <div className="texts">
-            <span>Nguyễn Long Duy</span>
-
-            <p>message.text</p>
-            <span>27 minute ago</span>
-          </div>
-        </div>
-        <div className="message">
+    <div className="form--messages">
+      <div className="form--messages-list">
+        <div className="form--messages-item">
           <img src={IMAGES.AVT_TEXT} />
           <div className="texts">
             <span>Nguyễn Long Duy</span>
@@ -32,22 +14,14 @@ const FormMessage = () => {
             <span>27 minute ago</span>
           </div>
         </div>
-        <div className="message">
-          <img src={IMAGES.AVT_TEXT} />
-          <div className="texts">
-            <span>Nguyễn Long Duy</span>
-            <p>message.text</p>
-            <span>27 minute ago</span>
-          </div>
-        </div>
-        <div className="message own">
+        <div className="form--messages-item own">
           <div className="texts">
             <p>message.text</p>
           </div>
           <img src={IMAGES.AVT_TEXT} />
         </div>
       </div>
-      <div className="bottom">
+      <div className="form--messages-chat">
         <div className="icons">
           <label htmlFor="file">
             <img src="./img.png" alt="" />
@@ -56,18 +30,9 @@ const FormMessage = () => {
           <img src="./camera.png" alt="" />
           <img src="./mic.png" alt="" />
         </div>
-        <input
-          type="text"
-          placeholder={'Type a message...'}
-          value={text}
-          onChange={(e) => setText(e.target.value)}
-        />
+        <input type="text" placeholder={'Type a message...'} />
         <div className="emoji">
-          <img
-            src="./emoji.png"
-            alt=""
-            onClick={() => setOpen((prev) => !prev)}
-          />
+          <img src="./emoji.png" alt="" />
           <div className="picker"></div>
         </div>
         <button className="sendButton">Send</button>
