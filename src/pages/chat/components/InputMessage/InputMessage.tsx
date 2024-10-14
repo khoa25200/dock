0ec69777fb6ef38520d937b1f6d3614f25a3294d
NguyenHaAnh <haanhnguyen1221@gmail.com>
2024-10-14 23:22:08 +0700

    Add: press enter to send

--- src/pages/chat/components/InputMessage/InputMessage.tsx
+++ src/pages/chat/components/InputMessage/InputMessage.tsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import './InputMessage.less';
 import { ICONS } from '../../../../assets/icons';
+import EmojiPicker from 'emoji-picker-react';
 
 function InputMessage({
   message,
@@ -12,23 +13,31 @@ function InputMessage({
   const [text, setText] = useState('');
   const [open, setOpen] = useState(false);
 
+  const handleKeyPress = (e) => {
+    if (e.key === 'Enter') {
+      setMessage([...message, text]);
+      setText('');
+    }
+  };
+
   return (
     <div className="bottom">
       <div className="icons">
         <label htmlFor="file">
-          <img src={ICONS.PAPER_CLIP} alt="" />
+          <img src={ICONS.PAPER_CLIP} alt="paper-clip" />
         </label>
         <input type="file" id="file" style={{ display: 'none' }} />
       </div>
+      {open ? <EmojiPicker /> : <></>}
       <div className="emoji icons">
-        <img src={ICONS.REACTION} alt="" onClick={() => setOpen((prev) => !prev)} />
-        <div className="picker"></div>
+        <img src={ICONS.REACTION} alt="reaction" onClick={() => setOpen((prev) => !prev)} />
       </div>
       <input
         type="text"
         placeholder={'Type a message...'}
         value={text}
         onChange={(e) => setText(e.target.value)}
+        onKeyPress={handleKeyPress}
       />
 
       <button className="sendButton icons" onClick={() => setMessage([...message, text])}>
