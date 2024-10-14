ce5afdcfe1730507ba7e460cfc2931e88aae7314
NguyenHaAnh <haanhnguyen1221@gmail.com>
2024-10-14 10:10:02 +0700

    Style: Input

---
+++ src/pages/chat/components/InputMessage/InputMessage.tsx
@@ -0,0 +1,41 @@
+import { useState } from 'react';
+import './InputMessage.less';
+import { ICONS } from '../../../../assets/icons';
+
+function InputMessage({
+  message,
+  setMessage,
+}: {
+  message: string[];
+  setMessage: (text: string[]) => void;
+}) {
+  const [text, setText] = useState('');
+  const [open, setOpen] = useState(false);
+
+  return (
+    <div className="bottom">
+      <div className="icons">
+        <label htmlFor="file">
+          <img src={ICONS.PAPER_CLIP} alt="" />
+        </label>
+        <input type="file" id="file" style={{ display: 'none' }} />
+      </div>
+      <div className="emoji icons">
+        <img src={ICONS.REACTION} alt="" onClick={() => setOpen((prev) => !prev)} />
+        <div className="picker"></div>
+      </div>
+      <input
+        type="text"
+        placeholder={'Type a message...'}
+        value={text}
+        onChange={(e) => setText(e.target.value)}
+      />
+
+      <button className="sendButton icons" onClick={() => setMessage([...message, text])}>
+        <img src={ICONS.PLANE} alt="" />
+      </button>
+    </div>
+  );
+}
+
+export default InputMessage;
