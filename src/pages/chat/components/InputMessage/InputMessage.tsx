40130c68789c18747a613cf9ba5d80e9b138d4ef
dangkhoa <dangkhoa@doctogo.vn>
2024-10-14 10:52:43 +0700

    cmt: add TODO(NHA)

--- src/pages/chat/components/InputMessage/InputMessage.tsx
+++ src/pages/chat/components/InputMessage/InputMessage.tsx
@@ -18,6 +18,7 @@ function InputMessage({
         <label htmlFor="file">
           <img src={ICONS.PAPER_CLIP} alt="" />
         </label>
+        {/* TODO (NHA): Add components handle preview file: Image, File(optional) */}
         <input type="file" id="file" style={{ display: 'none' }} />
       </div>
       <div className="emoji icons">
@@ -31,6 +32,7 @@ function InputMessage({
         onChange={(e) => setText(e.target.value)}
       />
 
+      {/* TODO (NHA): Handle Enter event for submit */}
       <button className="sendButton icons" onClick={() => setMessage([...message, text])}>
         <img src={ICONS.PLANE} alt="" />
       </button>
--- src/pages/chat/components/MessagePanel/partial/FormMessage/FormMessage.tsx
+++ src/pages/chat/components/MessagePanel/partial/FormMessage/FormMessage.tsx
@@ -24,7 +24,7 @@ const FormMessage = () => {
   };
 
   const props = {
-    sender: SENDER.FRIEND,
+    sender: SENDER.OWNER, //TODO (NHA): Handle for owner
     avtUrl: '',
     name: name,
     text: message,
