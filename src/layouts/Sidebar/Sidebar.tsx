01fa59af7564cb4a30b9dc686f8c5c84e9a2548f
Quach Hai Bang <68978560+bangqh123@users.noreply.github.com>
2024-10-15 13:02:35 +0700

    update css open sidebar sub

--- src/layouts/Sidebar/Sidebar.tsx
+++ src/layouts/Sidebar/Sidebar.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Sidebar.less';
 import './Sidebar.media.less';
 import { IMAGES } from '../../assets/images';
@@ -6,13 +6,21 @@ import { ICONS } from '../../assets/icons';
 import SideBarItem from './partials/SideBarItem/SideBarItem';
 
 const SidebarLayout: React.FC = () => {
+  const [openSub, setOpenSub] = useState(false);
+
+  const showSideBarSub = () => {
+    setOpenSub(prevState => !prevState);
+  };
+
   return (
     <div className="sidebar-wrapper">
       <div className="sidebar-inner">
         <img src={IMAGES.LOGO} className="logo-dockchat" />
-        <div className="sidebar-workspace">
+        <div className="sidebar-workspace" onClick={showSideBarSub}>
           <img src={IMAGES.LOGO} className="logo-dockchat"/>
-          <SideBarItem />
+          <SideBarItem 
+            openSub={openSub}
+          />
         </div>
         <div className="sidebar-list-feature">
           <ul>
--- src/layouts/Sidebar/partials/SideBarItem/SideBarItem.tsx
+++ src/layouts/Sidebar/partials/SideBarItem/SideBarItem.tsx
@@ -3,29 +3,46 @@ import { IMAGES } from '../../../../assets/images'
 import './SideBarItem.less'
 import './SideBarItem.media.less'
 import React from 'react'
+import {useLocation, useNavigate} from 'react-router-dom';
 
-const SideBarItem: React.FC = () => {
+type TSidebarSubProps = {
+    openSub: boolean,
+}
+
+const SideBarItem: React.FC<TSidebarSubProps> = ({openSub}) => {
+    const navigate = useNavigate();
+    const location = useLocation();
+
+    const goWorksPase = (path: string) => {
+        navigate(path);
+    };
+
+    const items = [
+        { path: '/chat/#', label: 'KTC-FE-SLACK' },
+        { path: '/chat/another', label: 'Another Workspace' },
+        { path: '/chat/yetanother', label: 'Yet Another Workspace' },
+    ];
+
     return (
-        <div className='side-item'>
-            <ul className='side-item-workspase'>
-                <li className='side-item-workspase-item active'>
-                    <img src={IMAGES.LOGO} alt="" />
-                    <p>KTC-FE-SLACK</p>
-                </li>
-                <li className='side-item-workspase-item'>
-                    <img src={IMAGES.LOGO} alt="" />
-                    <p>KTC-FE-SLACK</p>
-                </li>
-                <li className='side-item-workspase-item'>
-                    <img src={IMAGES.LOGO} alt="" />
-                    <p>KTC-FE-SLACK</p>
-                </li>
+        <div className={`side-sub ${openSub ? "open" : ""}`}>
+            <ul className='side-sub-workspase'>
+                {items.map((item, index) => (
+                    <li
+                        key={index}
+                        // className={`side-sub-workspase-item ${location.pathname === item.path ? 'active' : ''}`}
+                        className={`side-sub-workspase-item ${location.pathname === item.path || (location.pathname === '/chat/' && item.path === '/chat/#') ? 'active' : ''}`}
+                        onClick={() => goWorksPase(item.path)}
+                    >
+                        <img src={IMAGES.LOGO} alt="" />
+                        <p>{item.label}</p>
+                    </li>
+                ))}
             </ul>
-            <div className='side-item-add'>
-                <div className='side-item-add-icon'>
+            <div className='side-sub-add'>
+                <div className='side-sub-add-icon'>
                     <img src={ICONS.PLUS} alt="Plus" />
                 </div>
-                <p className='side-item-add-text'>Add a workspace</p>
+                <p className='side-sub-add-text'>Add a workspace</p>
             </div>
         </div>
     )
