8a4a88cba459e438fa1fcbe96427ef3eb3474d5e
Nguyễn Long Duy <88131939+Duy2401@users.noreply.github.com>
2024-10-17 00:16:24 +0700

    fix: chat details

--- src/pages/chat/components/MessagePanel/partial/MediasFiles/MediasFiles.tsx
+++ src/pages/chat/components/MessagePanel/partial/MediasFiles/MediasFiles.tsx
@@ -1,69 +1,69 @@
 import './MediasFiles.less';
-import { Avatar, Divider, List, Skeleton } from 'antd';
-import { useState } from 'react';
-import { ICONS } from '../../../../../../assets/icons';
-interface DataType {
-  nameFile: string;
-  capacity: string;
-  type: string;
-  picture: string;
-}
+import { Tabs } from 'antd';
+import type { TabsProps } from 'antd';
+import Documents from './components/Documents/Documents';
+import PhotoVideo from './components/Photo-Video/PhotoVideo';
+
+// This is the array of document data
+const documentData = [
+  {
+    nameFile: 'Screenshot-3817.png',
+    type: 'PNG',
+    capacity: '200gb',
+    picture:
+      'https://img.freepik.com/premium-photo/boy-with-green-eyes-green-eyes_922700-8.jpg?w=740',
+  },
+];
+const photovideoData = [
+  {
+    link_file:
+      'https://img.freepik.com/premium-photo/boy-with-green-eyes-green-eyes_922700-8.jpg?w=740',
+    date_receive: '20/10/2024;',
+  },
+  {
+    link_file:
+      'https://img.freepik.com/premium-photo/boy-with-green-eyes-green-eyes_922700-8.jpg?w=740',
+    date_receive: '20/10/2024;',
+  },
+  {
+    link_file:
+      'https://img.freepik.com/premium-photo/boy-with-green-eyes-green-eyes_922700-8.jpg?w=740',
+    date_receive: '20/10/2024;',
+  },
+  {
+    link_file:
+      'https://img.freepik.com/premium-photo/boy-with-green-eyes-green-eyes_922700-8.jpg?w=740',
+    date_receive: '20/10/2024;',
+  },
+  {
+    link_file:
+      'https://img.freepik.com/premium-photo/boy-with-green-eyes-green-eyes_922700-8.jpg?w=740',
+    date_receive: '20/10/2024;',
+  },
+];
+const items: TabsProps['items'] = [
+  {
+    key: '1',
+    label: 'Documents',
+    children: <Documents documents={documentData} />,
+  },
+  {
+    key: '2',
+    label: 'Photos and videos',
+    children: <PhotoVideo photovideo={photovideoData} />,
+  },
+];
+
+const onChange = (key: string) => {
+  console.log(key);
+};
+
 const MediaFiles = () => {
-  const TeamMember = [
-    {
-      nameFile: 'Screenshot-3817.png',
-      type: 'PNG',
-      capacity: '200gb',
-      picture:
-        'https://img.freepik.com/premium-photo/boy-with-green-eyes-green-eyes_922700-8.jpg?w=740',
-    },
-  ];
-  const [data, setData] = useState<DataType[]>(TeamMember);
   return (
-    <div className="files--container">
-      <div className="files--container-heading">
-        <div className="file--list">
-          <h1>Documents</h1>
-          <div className="files--count">
-            <span>6</span>
-          </div>
-        </div>
-        <div className="media--list">
-          <h1>Photo and video</h1>
-          <div className="medias--count">
-            <span>6</span>
-          </div>
-        </div>
-      </div>
-      <div
-        id="scrollableDiv"
-        style={{
-          maxHeight: 250,
-          height: '100%',
-          overflow: 'auto',
-        }}
-      >
-        <List
-          dataSource={data}
-          renderItem={(item) => (
-            <List.Item key={item.type}>
-              <List.Item.Meta
-                avatar={<Avatar src={item.picture} />}
-                title={item.nameFile}
-                description={
-                  <div>
-                    {item.type} - {item.capacity}
-                  </div>
-                }
-              />
-              <div className="media--icon-download">
-                <img src={ICONS.DOWNLOAD} />
-              </div>
-            </List.Item>
-          )}
-        />
-      </div>
+    <div className="media-file">
+      <Tabs defaultActiveKey="1" items={items} onChange={onChange} />
     </div>
   );
 };
+
 export default MediaFiles;
