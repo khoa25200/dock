e74d52f666d0e2d4172b68cb5b2d809f916b5d2b
Quach Hai Bang <68978560+bangqh123@users.noreply.github.com>
2024-10-16 21:30:03 +0700

    create setting user section

---
+++ src/layouts/Sidebar/partials/UserSetting/UserSetting.tsx
@@ -0,0 +1,35 @@
+import { ICONS } from '../../../../assets/icons'
+import { IMAGES } from '../../../../assets/images'
+import './UserSetting.less'
+import './UserSetting.media.less'
+
+import React from 'react'
+
+const UserSetting: React.FC = () => {
+    return (
+        <div className='setting'>
+            <div className='setting-user'>
+                <div className='setting-user-avatar'>
+                    <img src={IMAGES.AVT_TEXT} alt="Avatar" />
+                </div>
+                <div className='setting-user-infor'>
+                    <div className='user-infor-name'>
+                        Current - user - login
+                    </div>
+                    <div className='user-infor-status'>
+                        <img src={ICONS.ACTIVE} alt="Active" />
+                        <p>Active</p>
+                    </div>
+                </div>
+            </div>
+            <div className='setting-action'>
+                <div id='profile' className='setting-action-items'>Profile</div>
+                <div id='preference' className='setting-action-items'>Preferences</div>
+                <div id='signup-workspace' className='setting-action-items'>Sign out of KTC-FE-Basic</div>
+                <div id='signup-account' className='setting-action-items'>Sign out account</div>
+            </div>
+        </div>
+    )
+}
+
+export default UserSetting;
