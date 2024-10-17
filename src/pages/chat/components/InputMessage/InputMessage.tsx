d3daaf340abdc5f3ac1e71c7442f873f11d2c207
NguyenHaAnh <haanhnguyen1221@gmail.com>
2024-10-17 11:45:11 +0700

    Feat: button reply

--- src/pages/chat/components/InputMessage/InputMessage.tsx
+++ src/pages/chat/components/InputMessage/InputMessage.tsx
@@ -1,4 +1,4 @@
-import { ChangeEvent, useState } from 'react';
+import { ChangeEvent, useEffect, useState } from 'react';
 import './InputMessage.less';
 import { ICONS } from '../../../../assets/icons';
 import Picker, { EmojiClickData } from 'emoji-picker-react';
@@ -7,15 +7,22 @@ import { TextArea } from '@ant-design/pro-chat/es/components/Input';
 function InputMessage({
   message,
   setMessage,
+  reply,
+  setReply,
 }: {
   message: string[];
   setMessage: (text: string[]) => void;
+  reply: string;
+  setReply: (text: string) => void;
 }) {
   const [text, setText] = useState('');
   const [open, setOpen] = useState(false);
   const [chosenEmoji, setChosenEmoji] = useState<EmojiClickData | null>(null);
   const [image, setImage] = useState<string | null>(null);
   const [imageName, setImageName] = useState<string | null>(null);
+  const [file, setFile] = useState<File | null>(null);
+  const [fileName, setFileName] = useState<string | null>(null);
+  const [replyText, setReplyText] = useState<string>('');
 
   const handleKeyPress = (e: React.KeyboardEvent<HTMLTextAreaElement>) => {
     if (e.key === 'Enter') {
@@ -23,10 +30,7 @@ function InputMessage({
         return;
       } else {
         e.preventDefault();
-        if (text.trim() !== '') {
-          setMessage([...message, text]);
-          setText('');
-        }
+        handleSendMessage();
       }
     }
   };
@@ -39,27 +43,99 @@ function InputMessage({
   };
 
   const onImageChange = (e: ChangeEvent<HTMLInputElement>) => {
+    if (file) {
+      setFile(null);
+      setFileName(null);
+    }
     if (e.target.files && e.target.files[0]) {
       setImage(URL.createObjectURL(e.target.files[0]));
       setImageName(e.target.files[0].name);
     }
   };
 
+  const onFileChange = (e: ChangeEvent<HTMLInputElement>) => {
+    if (image) {
+      setImage(null);
+      setImageName(null);
+    }
+    if (e.target.files && e.target.files[0]) {
+      setFile(e.target.files[0]);
+      setFileName(e.target.files[0].name);
+    }
+  };
+
+  const handleSendMessage = () => {
+    if (file) {
+      console.log(true);
+      setFile(null);
+      setFileName(null);
+      if (text.trim() !== '') {
+        setMessage([...message, text]);
+        setText('');
+      }
+    }
+
+    if (image) {
+      setImage(null);
+      setImageName(null);
+      if (text.trim() !== '') {
+        setMessage([...message, text]);
+        setText('');
+      }
+    }
+
+    if (replyText) {
+      setReplyText('');
+      if (text.trim() !== '') {
+        setMessage([...message, text]);
+        setReply('');
+        setReplyText('');
+      }
+    }
+
+    if (text.trim() !== '') {
+      setMessage([...message, text]);
+      setText('');
+    }
+  };
+
+  useEffect(() => {
+    if (reply) {
+      setReplyText(reply);
+    }
+  }, [reply]);
+
   return (
     <div className="bottom">
+      {/* For file */}
       <div className="icons">
         <label htmlFor="file">
           <img src={ICONS.PAPER_CLIP} alt="paper-clip" />
         </label>
-        {/* TODO (NHA): Add components handle preview file: Image, File(optional) */}
         <input
           type="file"
           id="file"
-          accept="image/*"
+          accept="*"
+          style={{ display: 'none' }}
+          onChange={onFileChange}
+        />
+      </div>
+
+      {/* For media file */}
+      <div className="icons">
+        <label htmlFor="send-media-file">
+          <img src={ICONS.SEND_MEDIA_FILE} alt="send-media-file" />
+        </label>
+        <input
+          type="file"
+          id="send-media-file"
+          accept="image/*, video/*"
           style={{ display: 'none' }}
           onChange={onImageChange}
         />
       </div>
+
+      {/* For emoji */}
       <div className="emoji icons">
         {open && (
           <Picker
@@ -70,6 +146,8 @@ function InputMessage({
         )}
         <img src={ICONS.REACTION} alt="reaction" onClick={() => setOpen((prev) => !prev)} />
       </div>
+
+      {/* For image and text */}
       <div className="input-and-preview">
         {image && (
           <div className="image-preview">
@@ -82,6 +160,33 @@ function InputMessage({
             />
           </div>
         )}
+        {file && (
+          <div className="file-preview">
+            <img src={ICONS.FILE} alt={fileName || 'preview'} className="preview-file" />
+            <div className="file-name">{fileName}</div>
+            <img
+              src={ICONS.DELETE}
+              alt="delete-file"
+              className="delete-image"
+              onClick={() => setFile(null)}
+            />
+          </div>
+        )}
+        {replyText && (
+          <div className="reply-text">
+            <span>Reply: </span>
+            <span>{replyText}</span>
+            <img
+              src={ICONS.DELETE}
+              alt="delete-file"
+              className="delete-image"
+              onClick={() => {
+                setReply('');
+                setReplyText('');
+              }}
+            />
+          </div>
+        )}
         <TextArea
           placeholder="Type a message..."
           autoSize
@@ -91,7 +196,7 @@ function InputMessage({
         />
       </div>
 
-      <button className="sendButton icons" onClick={() => setMessage([...message, text])}>
+      <button className="sendButton icons" onClick={handleSendMessage}>
         <img src={ICONS.PLANE} alt="" />
       </button>
     </div>
