0605332431e7518af7dcdc86f1ef0cf92bacdd37
Nguyễn Long Duy <88131939+Duy2401@users.noreply.github.com>
2024-10-17 15:54:17 +0700

    create method work with api sign up

--- src/components/forms/FormSignUp/FormSignUp.tsx
+++ src/components/forms/FormSignUp/FormSignUp.tsx
@@ -1,56 +1,79 @@
-import './FormSignUp.less'
-import './FormSignUp.media.less'
+import './FormSignUp.less';
+import './FormSignUp.media.less';
 import React from 'react';
+import axios from 'axios';
 import { Checkbox, Form, Input, Layout } from 'antd';
-import SignUpButton from '../../buttons/ButtonAccount/ButtonAccount'
+import SignUpButton from '../../buttons/ButtonAccount/ButtonAccount';
 import SignUpGoogle from '../../buttons/ButtonAccountGoogle/ButtonAccountGoogle';
 
 type FieldType = {
-    fullname?: string;
-    email?: string;
-    password?: string;
-}; 
+  username?: string;
+  email?: string;
+  password?: string;
+};
 
 const FormSignUpPage: React.FC = () => {
-    return ( 
-        <Layout className="signup">
-            <div className='signup-title'>
-                <h1 className='signup-title-name'>Sign Up</h1>
-            </div>
-            <Form className='signup-from'>
-            <Form.Item<FieldType>
-                    className='signup-from-name'
-                    name="fullname"
-                    rules={[{ required: true, message: 'Please input your fullname!' }]}
-                >
-                    <Input placeholder='Enter FullName'/>
-                </Form.Item><Form.Item<FieldType>
-                    className='signup-from-email'
-                    name="email"
-                    rules={[{ required: true, message: 'Please input your email!' }]}
-                >
-                    <Input placeholder='Enter Email'/>
-                </Form.Item>
-                <Form.Item<FieldType>
-                    className='signup-from-password'
-                    name="password"
-                    rules={[{ required: true, message: 'Please input your password!' }]}
-                >
-                    <Input.Password placeholder='Enter Password'/>
-                </Form.Item>
-            </Form>
-            <Checkbox className='signup-checkbox'>I accept the Terms and Conditions.</Checkbox>
-            <div className='signup-button'>
-                <SignUpButton className='signup-button-account' title='Sign Up Account'/>
-                <div className='signup-button-or'>OR</div>
-                <SignUpGoogle className='signup-button-google' title='Continue with Google' />
-            </div>
-            <div className='signup-content'>
-                <span>Already using DockChat?</span>
-                <span>Sign in to an existing workspace</span>
-            </div>
-        </Layout>
+  const [formAccountUser] = Form.useForm();
+
+  const handleSubmitAccount = async () => {
+    const formData = formAccountUser.getFieldsValue();
+    const data = await axios.post(
+      'http://47.129.183.26:8080/api/auth/sign-up',
+      formData
     );
-}
+    console.log(data);
+
+    formAccountUser.resetFields();
+  };
+  return (
+    <Layout className="signup">
+      <div className="signup-title">
+        <h1 className="signup-title-name">Sign Up</h1>
+      </div>
+      <Form className="signup-from" form={formAccountUser}>
+        <Form.Item<FieldType>
+          className="signup-from-name"
+          name="username"
+          rules={[{ required: true, message: 'Please input your fullname!' }]}
+        >
+          <Input placeholder="Enter FullName" />
+        </Form.Item>
+        <Form.Item<FieldType>
+          className="signup-from-email"
+          name="email"
+          rules={[{ required: true, message: 'Please input your email!' }]}
+        >
+          <Input placeholder="Enter Email" />
+        </Form.Item>
+        <Form.Item<FieldType>
+          className="signup-from-password"
+          name="password"
+          rules={[{ required: true, message: 'Please input your password!' }]}
+        >
+          <Input.Password placeholder="Enter Password" />
+        </Form.Item>
+      </Form>
+      <Checkbox className="signup-checkbox">
+        I accept the Terms and Conditions.
+      </Checkbox>
+      <div className="signup-button">
+        <SignUpButton
+          className="signup-button-account"
+          title="Sign Up Account"
+          onclick={handleSubmitAccount}
+        />
+        <div className="signup-button-or">OR</div>
+        <SignUpGoogle
+          className="signup-button-google"
+          title="Continue with Google"
+        />
+      </div>
+      <div className="signup-content">
+        <span>Already using DockChat?</span>
+        <span>Sign in to an existing workspace</span>
+      </div>
+    </Layout>
+  );
+};
 
 export default FormSignUpPage;
