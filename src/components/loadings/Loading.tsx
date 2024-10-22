7d9ed39c162ac0e432766c16416afc380a87ba78
Quach Hai Bang <68978560+bangqh123@users.noreply.github.com>
2024-10-22 14:45:13 +0700

    create loading component

---
+++ src/components/loadings/Loading.tsx
@@ -0,0 +1,38 @@
+import './Loading.less'
+
+import React, { useState, useEffect } from 'react';
+import { Flex, Spin } from 'antd';
+
+type TLoadingProps = {
+    isLoading: boolean
+}
+
+const content = <div />;
+
+const Loading:React.FC<TLoadingProps> = ({isLoading}) => {
+    const [loading, setLoading] = useState(isLoading);
+
+    useEffect(() => {
+        const timer = setTimeout(() => {
+          setLoading(false);
+        }, 500);
+    
+        return () => clearTimeout(timer);
+    }, []);
+    
+    return (
+        <Flex gap="middle" vertical>
+            <Spin 
+                fullscreen 
+                // delay={500}
+                size="large"
+                spinning={loading} 
+                tip="Data Loading ..." 
+            >
+                {content}
+            </Spin>
+        </Flex>
+    )
+}
+
+export default Loading;
