06802965c627d42e618af70501448e29e7efb242
Quach Hai Bang <68978560+bangqh123@users.noreply.github.com>
2024-10-28 21:05:46 +0700

    create file workspace.config

---
+++ src/configs/constants/workspace.config.ts
@@ -0,0 +1,11 @@
+const workspaceProps = {
+    welcome: 'Welcome back',
+    workspacefor: 'Workspaces for',
+    createWorkspace: 'Want to create a different team?'
+}
+
+export const PROPS = {
+    WORKSPACE: {
+        ...workspaceProps,
+    }
+}
